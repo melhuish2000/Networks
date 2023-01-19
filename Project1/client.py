06c884c9010a4ac79f466c3287f48fd5ea3bafb1
melhuish2000 <melhuish.a@northeastern.edu>
2023-01-18 23:30:59 -0500

    init

---
+++ Project1/client.py
@@ -0,0 +1,88 @@
+import socket
+import json
+import ssl
+import sys
+
+
+
+def play_game(port: int, tls: bool, hostname: str, username: str):
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if tls:
+        context = ssl.create_default_context()
+        client = context.wrap_socket(client, server_hostname = hostname)
+    server = ('localhost', 8080)
+
+    client.connect(server)
+    send(client, get_hello_message())
+    start_message = recieve(client)
+    id = start_message["id"]
+    print(id)
+
+
+    
+
+
+def get_hello_message(username):
+    return {"type": "hello", "northeastern_username": username}
+
+
+def get_guess_message(guess, game_id):
+    return {"type": "guess", "id": game_id, "word": guess}
+
+
+#formats and sends
+def send(client, dictionary):
+    data = json.dumps(dictionary)
+    encoded = f"{data}\n".encode("utf-8")
+    client.send(encoded)
+
+
+
+#Recieve
+def recieve(client) -> dict:
+    return json.loads(client.recv(1000000).decode("utf-8").strip())
+
+#python dictionary exampel
+
+
+dictionary = dict()
+
+dictionary["a"] = 5
+dictionary[0] = 7
+
+
+dictionary2 = {
+    "key1": "value1",
+    "key2": 2
+}
+
+print(dictionary2["key1"]) # value1
+
+if __name__ == "__main__":
+    counter = 1
+
+    port = 27993
+    tls = False
+    use_default_port = True
+
+    if sys.argsv[counter] == "-p":
+        port = int(sys.arv[counter])
+        counter += 1
+        use_default_port = False
+
+    if sys.argv[counter] == "-s":
+        tls = True
+        if use_default_port:
+            port = 27994
+        
+    hostname = sys.argv[counter]
+    counter+=1
+    username = sys.argv[counter]
+
+    play_game(port, tls, hostname, username)
+
+
+
+
+    
